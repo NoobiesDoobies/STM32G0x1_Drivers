ee55a439f04babd25ca93e4be4984fa07859e905
NoobiesDoobies <carlisoeryan20@gmail.com>
2025-02-01 17:00:35 +0700

    config add driver include path

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -19,7 +19,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32G0 -DNUCLEO_G0B1RE -DSTM32G0B1RETx -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32G0 -DNUCLEO_G0B1RE -DSTM32G0B1RETx -c -I../Inc -I/home/carlios/STM32CubeIDE/workspace_1.17.0/STM32G0x1_Drivers/drivers/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Src
 
---
+++ drivers/Inc/stm32g0x1.h
@@ -0,0 +1,13 @@
+/*
+ * stm32g0x1.h
+ *
+ *  Created on: Feb 1, 2025
+ *      Author: carlios
+ */
+
+#ifndef INC_STM32G0X1_H_
+#define INC_STM32G0X1_H_
+
+
+
+#endif /* INC_STM32G0X1_H_ */
