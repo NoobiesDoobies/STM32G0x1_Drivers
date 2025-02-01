d0c2e9410d390b7562c4a9f34e8ce83d1d0c5f3a
NoobiesDoobies <carlisoeryan20@gmail.com>
2025-02-01 16:54:36 +0700

    config debug print

--- Src/main.c
+++ Src/main.c
@@ -17,13 +17,20 @@
  */
 
 #include <stdint.h>
+#include <stdio.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+extern void initialise_monitor_handles(void);
+
 int main(void)
 {
+	initialise_monitor_handles();
+
+	printf("halooo\n");
+
     /* Loop forever */
 	for(;;);
 }
